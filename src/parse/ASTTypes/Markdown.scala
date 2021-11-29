b1758004aca044badc05ce8ac56835bf27061a6f
Matthew Pereira <mbpereira49@gmail.com>
2021-11-29 11:15:05 -0600

    reorganized

---
+++ src/parse/ASTTypes/Markdown.scala
@@ -0,0 +1,22 @@
+enum Header:
+  case H1, H2, H3
+enum Format:
+  case Bold, Italics, Link
+
+sealed trait Text
+object Text:
+  case class Plain(s: String) extends Text
+  case class Formatted(s: Line, f: Format) extends Text
+
+sealed trait Line
+object Line:
+  case class Line_(l: Seq[Text]) extends Line
+
+sealed trait Block
+object Block:
+  case class P(ls: Seq[Line]) extends Block // paragraph
+  case class Hdr(l: Line, h: Header) extends Block // header
+
+sealed trait Body
+object Body:
+  case class Blocks(l: List[Block]) extends Body
